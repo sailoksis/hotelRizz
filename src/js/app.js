def9b7dcdbe03f47e9dc258dd9243db4074db66f
sailoksis <53906219+sailoksis@users.noreply.github.com>
2022-08-29 06:34:03 +0300

    Тип кнопки исправлен на submit
    Тип кнопки исправлен на submit

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,6 @@
 import * as flsFunctions from "./modules/functions.js";
-
+import documentActions from "./modules/script.js";
 
 flsFunctions.isWebp();
+documentActions.function();//Нужно добавить _hover элементу menu__item  при нажатии на стрелку и какт правильно импортировать функцию 
+
